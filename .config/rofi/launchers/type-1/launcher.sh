50ba16f395a22731c6c77d626c55b97712737f74
kizilkayabayram <bayram.kizilkaya2017@gtu.edu.tr>
2022-12-28 12:12:22 +0300

    switch super + tab for windows

--- .config/rofi/launchers/type-1/launcher.sh
+++ .config/rofi/launchers/type-1/launcher.sh
@@ -15,6 +15,11 @@ dir="$HOME/.config/rofi/launchers/type-1"
 theme='style-8'
 
 ## Run
+xdotool search --sync --syncsleep 50 --limit 1 --class Rofi keyup --delay 0 Tab key --delay 0 Tab keyup --delay 0 Super_L keydown --delay 0 Super_L&
 rofi \
     -show window \
-    -theme ${dir}/${theme}.rasi
+    -theme ${dir}/${theme}.rasi \
+    -kb-cancel "Super+Escape,Escape" \
+    -kb-accept-entry "!Super-Tab,!Super_L,!Super+Super_L,Return"\
+    -kb-row-down "Super+Tab,Super+Down" \
+    -kb-row-up "Super+ISO_Left_Tab,Super+Shift+Tab,Super+Up"&
